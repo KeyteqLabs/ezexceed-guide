8d2e45f9d74431d50877e9e2b9a3988d33cc533a
Raymond Julin <raymond.julin@gmail.com>
2013-05-03 14:30:45 +0200

    Add web server + compile all to 1 html file

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,9 +8,28 @@ module.exports = function(grunt) {
                 files: 'src/*.md',
                 dest: 'docs/'
             }
+        },
+
+
+        connect: {
+            docs: {
+                options: {
+                    port: 9092,
+                    base: 'docs/'
+                }
+            }
+        },
+
+        watch: {
+            docs: {
+                files: 'src/*',
+                tasks: ['markdown']
+            }
         }
     });
     
     grunt.loadNpmTasks('grunt-markdown');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.registerTask('default', ['markdown']);
 };
